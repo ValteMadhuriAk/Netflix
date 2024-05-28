80f173fc958a0e29298e6205efed08ed505e1252
madhudab <madhuridabhade16@gmail.com>
2024-05-28 18:40:32 +0530

    sign/signup form && K=Login/Header

---
+++ src/components/Body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Login from './Login'
+
+const Body = () => {
+  return (
+    <div>
+        <Header />
+        <Login /></div>
+  )
+}
+
+export default Body
