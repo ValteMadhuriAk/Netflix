329b3e659daec0f47a02b4e7a50ac85e6f2537c0
madhudab <madhuridabhade16@gmail.com>
2024-06-16 11:41:21 +0530

    Built Multi language feature along with GPT search

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -6,7 +6,7 @@ const SecondaryContainer = () => {
   const movies = useSelector((store)=>store.movies);
   return (
     <div className='bg-black'>
-    <div className='-mt-52 relative z-20'>
+    <div className=' -mt-80 relative z-20'>
       <MovieList title={"Now Playing"}  moviesList={movies.nowPlayingMovies}/>
       <MovieList title={"Top Rated"}  moviesList={movies.topRatedMovies}/>
       <MovieList title={"Popular"}  moviesList={movies.upcomingMovies}/>
